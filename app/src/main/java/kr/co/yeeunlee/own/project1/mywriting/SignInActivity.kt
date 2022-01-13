76df8d7a21ac30ac2ed185d65885dc5f5149e3e4
Yeeun Lee <leeyeah8245@gmail.com>
2022-01-14 02:00:42 +0900

    Complete signin UI

--- app/src/main/java/kr/co/yeeunlee/own/project1/mywriting/SignInActivity.kt
+++ app/src/main/java/kr/co/yeeunlee/own/project1/mywriting/SignInActivity.kt
@@ -30,27 +30,23 @@ class SignInActivity : AppCompatActivity() {
 
 
         binding.btnNameDupli.setOnClickListener {
-            var name = binding.editLayoutName.editText?.text
+            var name = binding.editName.text
             duplicateName(name.toString())
         }
         binding.btnComplete.setOnClickListener {
-            completeEmail(binding.editLayoutEmail.editText?.text.toString())
-
-
-
-
+            completeEmail(binding.editEmail.text.toString())
         }
-        binding.editLayoutName.editText?.addTextChangedListener(listnerEditName)
+        binding.editName.addTextChangedListener(listnerEditName)
     }
 
     private fun duplicateName (name: String){
         // 키보드 내리고 포커스 없애기
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(binding.editLayoutName.editText?.windowToken,0)
-        binding.editLayoutName.editText?.clearFocus()
+        imm.hideSoftInputFromWindow(binding.editName.windowToken,0)
+        binding.editName.clearFocus()
 
         if (limitName == false){
-            binding.editLayoutName.error = "특수문자, 공백 제외 7 글자 이하"
+            binding.editName.error = "특수문자, 공백 제외 7 글자 이하"
             return
         }
         db.collection("check").document("name").get()
@@ -60,13 +56,13 @@ class SignInActivity : AppCompatActivity() {
                 Log.d("별명",name.toString())
                 if ((li.contains(name) == false) and (name != "") ) {
                     map["name"] = true
-                    binding.editLayoutName.error = null
+                    binding.editName.error = null
                     Toast.makeText(this,"가능한 별명입니다.", Toast.LENGTH_SHORT).show()
                 }
                 else if (name == "")
-                    binding.editLayoutName.error="별명을 입력해주세요"
+                    binding.editName.error="별명을 입력해주세요"
                 else // valid == false
-                    binding.editLayoutName.error="이미 존재하는 별명입니다."
+                    binding.editName.error="이미 존재하는 별명입니다."
                 Log.d("별명 valid","$name ${map["name"]}")
             }
             .addOnFailureListener { Log.d("별명 등록 실패","${it}") }
@@ -76,24 +72,24 @@ class SignInActivity : AppCompatActivity() {
         // 이메일 중복 확인
         var pattern = android.util.Patterns.EMAIL_ADDRESS
         if (pattern.matcher(email).matches()) { // 정규 이메일 맞음
-            binding.editLayoutEmail.error = null
+            binding.editEmail.error = null
             db.collection("user").document(email).addSnapshotListener { document, error ->
                 // 중복
                 Log.d("이메일 리스너", "${document!!.exists()}")
                 Log.d("이메일 리스너", "${error}")
                 if (document.exists()) {
                     map["email"] = false
-                    binding.editLayoutEmail.error = "이미 가입된 이메일 입니다."
+                    binding.editEmail.error = "이미 가입된 이메일 입니다."
                 } else {
                     Log.d("이메일 리스너", "${map}")
                     map["email"] = true
                     Log.d("이메일 리스너", "${map}")
                     if (map["name"] == false) {
-                        binding.editLayoutName.error = "별명 중복확인을 해주세요."
+                        binding.editName.error = "별명 중복확인을 해주세요."
                     }
                     completePassWord(
-                        binding.editLayoutPassWord.editText?.text.toString(),
-                        binding.editLayoutPassWordCheck.editText?.text.toString()
+                        binding.editPW.text.toString(),
+                        binding.editPWCheck.text.toString()
                     )
 
                     Log.d("인증메일전송", "$map")
@@ -103,9 +99,9 @@ class SignInActivity : AppCompatActivity() {
                         // 모두 true일 때 인증 이메일 전송
                         Log.d("인증메일전송1", "$map")
                         // start로 액티비티 전환 후 finish
-                        val inputName = binding.editLayoutName.editText?.text.toString()
-                        val inputEmail = binding.editLayoutEmail.editText?.text.toString()
-                        val inputPassword = binding.editLayoutPassWord.editText?.text.toString()
+                        val inputName = binding.editName.text.toString()
+                        val inputEmail = binding.editEmail.text.toString()
+                        val inputPassword = binding.editPW.text.toString()
                         val intentStart = Intent(this, LoginStartActivity::class.java)
                         user = User(inputName, inputEmail, true, inputPassword)
                         Log.d("인증메일전송2", "$user")
@@ -118,7 +114,7 @@ class SignInActivity : AppCompatActivity() {
             }
         }
         else
-            binding.editLayoutEmail.error = "올바른 이메일 형식을 입력해주세요."
+            binding.editEmail.error = "올바른 이메일 형식을 입력해주세요."
 
         return true
     }
@@ -126,17 +122,17 @@ class SignInActivity : AppCompatActivity() {
     private fun completePassWord(password:String, checkPassword:String){
         map["password"] = false
         if (password == "")
-            binding.editLayoutPassWord.error = "비밀번호를 입력해주세요."
+            binding.editPW.error = "비밀번호를 입력해주세요."
 
         if (checkPassword == "")
-            binding.editLayoutPassWordCheck.error = "비밀번호 확인을 입력해주세요."
+            binding.editPWCheck.error = "비밀번호 확인을 입력해주세요."
         else if (password.equals(checkPassword)) {
             map["password"] = true
-            binding.editLayoutPassWord.error = null
-            binding.editLayoutPassWordCheck.error = null
+            binding.editPW.error = null
+            binding.editPWCheck.error = null
         }
         else
-            binding.editLayoutPassWordCheck.error = "비밀번호를 다시 한번 정확히 입력해주세요."
+            binding.editPWCheck.error = "비밀번호를 다시 한번 정확히 입력해주세요."
     }
 
 
@@ -150,11 +146,11 @@ class SignInActivity : AppCompatActivity() {
             if (s != null){
                 if (pattern.matcher(s.toString()).matches()) {   // 한글, 영어, 숫자만 있을 때
                     limitName = true
-                    binding.editLayoutName.error = null
+                    binding.editName.error = null
                 }
                 else{
                     limitName = false
-                    binding.editLayoutName.error = "특수문자, 공백 제외 7 글자 이하"
+                    binding.editName.error = "특수문자, 공백 제외 7 글자 이하"
                 }
             }
         }
