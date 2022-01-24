7253a921028371800657f85ad0f8ff3ab8f4ef42
Yeeun Lee <leeyeah8245@gmail.com>
2022-01-24 19:22:39 +0900

    Create storage function

--- app/src/main/java/kr/co/yeeunlee/own/project1/mywriting/StorageFragment.kt
+++ app/src/main/java/kr/co/yeeunlee/own/project1/mywriting/StorageFragment.kt
@@ -1,12 +1,19 @@
 package kr.co.yeeunlee.own.project1.mywriting
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.viewModelScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kr.co.yeeunlee.own.project1.mywriting.databinding.FragmentStorageBinding
+import kr.co.yeeunlee.own.project1.mywriting.databinding.ItemStorageRecyclerBinding
 
 
 class StorageFragment : Fragment() {
@@ -15,6 +22,7 @@ class StorageFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -23,18 +31,78 @@ class StorageFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentStorageBinding.inflate(inflater, container, false)
-        initBtnBottle() // 저금통 생성 & 저금통을 누르면
+        // viewModel 초기화와 동시에 bottle 업데이트 한 번 됨
+        //initBtnBottle() // 저금통 생성 & 저금통을 누르면
+        //storageViewModel.oneMore()
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        storageViewModel.oneMore()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        storageViewModel.storageBottleSnap.observe(viewLifecycleOwner){
-            initBtnBottle()
-        }
+        binding.recyclerStorage.layoutManager = LinearLayoutManager(context)
+        binding.recyclerStorage.adapter = StorageAdapter(storageViewModel.stgBtSnapLi.value!!)
+
+        storageViewModel.stgBtSnapLi.observe(viewLifecycleOwner, Observer {
+            (binding.recyclerStorage.adapter as StorageAdapter).setData(it)
+        })
+
     }
 
-    private fun initBtnBottle(){}
+
+//    private fun initBtnBottle(){
+//        val firebaseRepository = FirebaseRepository()
+//        firebaseRepository.getStorageBottle()
+//    }
+
+    inner class StorageAdapter(private var dataLi: ArrayList<BottleList>):
+        RecyclerView.Adapter<StorageAdapter.ViewHolder>(){
+
+        inner class ViewHolder(view: ItemStorageRecyclerBinding): RecyclerView.ViewHolder(view.root){
+            val btnBottle1 = view.btnBottle1
+            val btnBottle2 = view.btnBottle2
+            val btnBottle3 = view.btnBottle3
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            Log.d("보틀 수3", parent.toString())
+            val view = ItemStorageRecyclerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            Log.d("보틀 수4", dataLi!!.get(position).toString())
+            dataLi!!.get(position).let { item ->
+                with(holder){
+                    if (item.first == null){
+                        btnBottle1.visibility = View.INVISIBLE
+                        btnBottle1.isEnabled = false
+                    }
+                    if (item.second == null){
+                        btnBottle2.visibility = View.INVISIBLE
+                        btnBottle2.isEnabled = false
+                    }
+                    if (item.third == null){
+                        btnBottle3.visibility = View.INVISIBLE
+                        btnBottle3.isEnabled = false
+                    }
+                }
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return storageViewModel.getSize()
+        }
+
+        fun setData(newData: ArrayList<BottleList>){
+            dataLi = newData
+            notifyDataSetChanged()
+        }
+    }
 
 }
--- app/src/main/java/kr/co/yeeunlee/own/project1/mywriting/StorageViewModel.kt
+++ app/src/main/java/kr/co/yeeunlee/own/project1/mywriting/StorageViewModel.kt
@@ -1,5 +1,7 @@
 package kr.co.yeeunlee.own.project1.mywriting
 
+import android.util.Log
+import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -9,14 +11,31 @@ import kotlinx.coroutines.launch
 
 class StorageViewModel:ViewModel() {
     private val firebaseRepo: FirebaseRepository = FirebaseRepository()
-    private var _storageBottleSnap: MutableLiveData<DocumentSnapshot> = MutableLiveData<DocumentSnapshot>()
-    val storageBottleSnap:LiveData<DocumentSnapshot> = _storageBottleSnap
+    private var __stgBottleLi = ArrayList<BottleList>()
+    private var _stgBottleLi: MutableLiveData<ArrayList<BottleList>> = MutableLiveData<ArrayList<BottleList>>()
+    val stgBtSnapLi:LiveData<ArrayList<BottleList>> = _stgBottleLi
 
     init {
-
+        _stgBottleLi.value = __stgBottleLi
+        //getStorageBottleLi()
     }
 
-    private fun getStorageBottleSnap() = viewModelScope.launch {
-        firebaseRepo.getStorageBottle(_storageBottleSnap)
+    fun getStorageBottleLi(){
+        firebaseRepo.getStorageBottle(_stgBottleLi, __stgBottleLi)
+    }
+
+    fun oneMore(){
+        getStorageBottleLi()
+    }
+
+    fun add(bottleList: BottleList){
+        __stgBottleLi.add(bottleList)
+        _stgBottleLi.value = __stgBottleLi
+    }
+
+    fun getSize(): Int{
+        return __stgBottleLi.size
     }
 }
+
+data class BottleList(var first: Int? = null, var second: Int? = null, var third: Int? = null)
