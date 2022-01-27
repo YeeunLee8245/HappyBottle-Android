e5785d73a61ab92759a6a02a58c2495abf83bcc2
Yeeun Lee <leeyeah8245@gmail.com>
2022-01-27 17:37:32 +0900

    Add send check function

--- app/src/main/java/kr/co/yeeunlee/own/project1/mywriting/SendFragment.kt
+++ app/src/main/java/kr/co/yeeunlee/own/project1/mywriting/SendFragment.kt
@@ -1,12 +1,15 @@
 package kr.co.yeeunlee.own.project1.mywriting
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.FragmentResultListener
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.MutableLiveData
 import com.google.firebase.firestore.DocumentSnapshot
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -16,11 +19,16 @@ import kr.co.yeeunlee.own.project1.mywriting.databinding.FragmentSendBinding
 
 class SendFragment : Fragment() {
     private lateinit var binding: FragmentSendBinding
+    private var vaild = MutableLiveData<Boolean>()
     private val firebaseRepo = FirebaseRepository()
     private val sendViewModel: SendViewModel by viewModels<SendViewModel>()
 
+    init {
+        vaild.value = false
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -29,6 +37,7 @@ class SendFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentSendBinding.inflate(inflater, container, false)
+
         initBtnSend() // 도착한 쪽지 터치
         return binding.root
     }
@@ -36,22 +45,27 @@ class SendFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        Log.d("Recreate","재배치")
+        vaild.observe(viewLifecycleOwner){
+            checkVaild(it)
+        }
 //        sendViewModel.sendSnapshot.observe(viewLifecycleOwner){
 //            initSend(it)
 //        }
     }
 
+
     private fun initBtnSend(){
         binding.btnSend.setOnClickListener {
             val dialog = SendDialogFragment()
             val firebaseRepo = FirebaseRepository()
+            vaild.value = false
             dialog.setSendBtnListener(object : SendDialogFragment.SendOnBtnClickListener{
-                override fun SendOnBtnClicked(receiver: String, textEditNote: String) {
-                    CoroutineScope(Dispatchers.Default).launch {
-                        if (true == firebaseRepo.setSendNoteAdd(receiver, textEditNote)) {
-                            Toast.makeText(context, "전송 완료", Toast.LENGTH_SHORT).show()
+                override fun SendOnBtnClicked(receiver: String, textEditNote: String){
+                    CoroutineScope(Dispatchers.Main).launch {
+                        firebaseRepo.setSendNoteAdd(receiver, textEditNote, vaild)
                             //TODO("서비스 추가하기(상대방에게 푸시 알림 전송)")
-                        }
+
                     }
                 }
             })
@@ -62,5 +76,10 @@ class SendFragment : Fragment() {
     }
 
     private fun initSend(snapshot: DocumentSnapshot){}
+
+    private fun checkVaild(result:Boolean){
+        if (result == true)
+            Toast.makeText(context, "전송 완료!", Toast.LENGTH_SHORT).show()
+    }
 
 }
