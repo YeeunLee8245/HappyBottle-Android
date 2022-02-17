a76b9ca7006f63574733dc2c066baf1acd56aed2
Yeeun Lee <leeyeah8245@gmail.com>
2022-02-18 01:07:58 +0900

    Fix memory leak in send fragment

--- app/src/main/java/kr/co/yeeunlee/own/project1/mywriting/SendViewModel.kt
+++ app/src/main/java/kr/co/yeeunlee/own/project1/mywriting/SendViewModel.kt
@@ -2,15 +2,18 @@ package kr.co.yeeunlee.own.project1.mywriting
 
 import android.app.Application
 import android.content.Context
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.firestore.ListenerRegistration
 import kotlinx.coroutines.launch
 import okhttp3.ResponseBody
 import retrofit2.Response
 
 class SendViewModel(application: Application): AndroidViewModel(application) {
+    private var listenerRgst:ListenerRegistration? = null
     private var _myResponce: MutableLiveData<Response<ResponseBody>> = MutableLiveData<Response<ResponseBody>>()
     val myResponce: LiveData<Response<ResponseBody>> = _myResponce
     private var __checkPost: ArrayList<Note> = ArrayList<Note>()
@@ -29,6 +32,8 @@ class SendViewModel(application: Application): AndroidViewModel(application) {
 
     fun getPostSnapshot() = viewModelScope.launch{
         val firebaseRepo = FirebaseRepository(mapplication)
-        firebaseRepo.getPostSnapshot(__checkPost,_checkPost)
+        if (listenerRgst == null)
+            listenerRgst = firebaseRepo.getPostSnapshot(__checkPost,_checkPost)
     }
+
 }
